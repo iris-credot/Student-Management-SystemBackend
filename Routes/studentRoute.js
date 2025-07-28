e4ec11391c816d0bdfcd4f982f00ba53dda1839d
iris-credot <tetairiscredot@gmail.com>
2025-07-29 01:44:41 +0200

    corrected the student controller

--- Routes/studentRoute.js
+++ Routes/studentRoute.js
@@ -4,13 +4,13 @@ const studentController = require('../Controller/studentController');
 const auth = require('../Middleware/Authenticator');
 
 
-
-router.get('/',auth.adminJWT, studentController.getAllStudents);
-router.get('/:id',auth.adminJWT, studentController.getStudentById);
 router.get('/active',auth.adminJWT, studentController.getActiveStudents);
 router.get('/dropped',auth.adminJWT, studentController.getDroppedStudents);
 router.get('/graduated',auth.adminJWT, studentController.getGraduatedStudents);
 router.post('/',auth.adminJWT, studentController.createStudent);
+router.get('/',auth.adminJWT, studentController.getAllStudents);
+router.get('/one/:id',auth.adminJWT, studentController.getStudentById);
+
 router.put('/:id',auth.adminJWT, studentController.updateStudent);
 router.delete('/:id',auth.adminJWT, studentController.deleteStudent);
 
